27e1d7ac4bf936ce82dea4583a2a0af01ef61ec4
Greg Dorshimer <greg.dorshimer@protonmail.com>
2024-11-12 17:45:48 -0500

    refactored for jquery

--- sudoku/static/sudoku/js/sudoku.js
+++ sudoku/static/sudoku/js/sudoku.js
@@ -1,24 +1,18 @@
-// TODO refactor to use jQuery
-
-var puzzle = ''
-var solution = ''
-
 const testPuzzle = "000000300010003780040100260400308020000204610002590400564000900001000000387002000";
 
 // pulls answers from the board and returns as a string with zeros for unfilled spaces
 function getAnswers() {
-  answers = ''
+  answers = '';
 
-  for (let i = 0; i < 81; i++) {
-    let id = "c" + i.toString();
-    if (document.getElementById(id).getAttribute("value")) {
-      answers += document.getElementById(id).getAttribute("value");
-    } else if (id.innerText) {
-      answers += document.getElementById(id).innerText;
+  $(".cell-text").each(function() {
+    if ($(this).attr("value")) {
+      answers += $(this).attr("value");
+    } else if ($(this).text()) {
+      answers += $(this).text();
     } else {
       answers += "0";
     }
-  }
+  });
 
   return answers;
 }
@@ -31,11 +25,13 @@ async function submit() {
   // TODO send http post to django api endpoint
   // TODO return response (either bool or [] or [rows/columns that are wrong])
   var response = "";
+  alertCorrect();
+  /*
   if (response.correct) {
     alertCorrect();
   } else {
     alertIncorrect(response.rows, response.columns);
-  }
+  } */
 }
 
 function alertCorrect() {
@@ -68,27 +64,31 @@ function alertIncorrect(rows, columns) {
 }
 
 function displayPuzzle(puzzle) {
+  $(".cell").removeClass("cell-correct");
+  $(".cell").removeClass("cell-incorrect");
+  $(".cell").addClass("cell-normal");
+
   for (let i = 0; i < 81; i++) {
     let id = "c" + i;
-    let element = document.getElementById(id);
+    let element = $("#" + id);
     if (puzzle[i] != 0) {
-      element.setAttribute("class", "cell-text f");
-      element.setAttribute("readonly", true);
-      element.setAttribute("value", "" + puzzle[i]);
-      element.removeAttribute("maxlength");
-      element.innerText = "";
+      element.removeClass("e");
+      element.addClass("f");
+      element.attr("readonly", "");
+      element.attr("value", puzzle[i]);
+      element.removeAttr("maxlength");
+      element.text("");
     }
     else {
-      element.setAttribute("class", "cell-text e");
-      element.removeAttribute("readonly");
-      element.setAttribute("value", "");
-      element.removeAttribute("value");
-      element.setAttribute("maxlength", "1");
-      element.innerText = "";
+      element.removeClass("f");
+      element.addClass("e");
+      element.removeAttr("readonly");
+      element.removeAttr("value");
+      element.attr("maxlength", "1");
+      element.text("");
     }
   }
 }
-
 
   /*
   url = "https://youdosudoku.com/api/"
@@ -117,7 +117,6 @@ function displayPuzzle(puzzle) {
 
 function buttonClick(button) {
   console.log('buttonClick(' + button + ')');
-
 
   // TODO: enable below once implemented
   // puzzle = /* await? */ getPuzzle(button);
