3da0949f8d833924d6a808b41dd80d4a0c557bff
Greg Dorshimer <greg.dorshimer@protonmail.com>
2024-11-20 17:05:58 -0500

    db testing

--- sudoku/models.py
+++ sudoku/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class TestModel(models.Model):
     test_string = models.CharField(max_length=20)
     test_datetime = models.DateTimeField("date")
-    test_field1 = models.CharField(max_length=20)
+    test_field1 = models.CharField(max_length=20,default="default") # 'default=' defines the value that is initialized for rows that existed before this field was migrated
 
     def __str__(self):
         return self.test_string
