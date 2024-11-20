fdc8e5827ef21c6e88c46aaaa8866938e03606e7
Greg Dorshimer <greg.dorshimer@protonmail.com>
2024-11-20 16:49:04 -0500

    db testing

--- sudoku/models.py
+++ sudoku/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class TestModel(models.Model):
     test_string = models.CharField(max_length=20)
     test_datetime = models.DateTimeField("date")
+    test_field1 = models.CharField(max_length=20)
 
     def __str__(self):
         return self.test_string
