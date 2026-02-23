b0f420cac790988ca69535b26a65fc1ab9fc8555
Greg Dorshimer <greg.dorshimer@protonmail.com>
2026-02-23 18:20:34 -0500

    cleanup

--- webapp/views.py
+++ webapp/views.py
@@ -6,6 +6,9 @@ from webapp.settings import RESUME_DIR
 def index(request):
     #return HttpResponse('Hello, world. You're at the dictionary index.')
     #context = {'latest_question_list': latest_question_list}
+    # source_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR'))
+    print('### Request from:', ip)
     return render(request, "index.html", )
 
 def resume(request):
